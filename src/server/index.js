be362ec850e127e7d02f70865d50341dd98fc503
J0NNYZER0 <jon.ortiz@me.com>
2018-10-05 11:38:58 -0700

    change process to heroku friendly in serverjs

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ import App from '../shared/App'
 // Create Hapi server
 const Server = Hapi.server({
     host:'0.0.0.0',
-    port:8000,
+    port: process.env.PORT || 5000,
     state: { ignoreErrors: true }
 })
 
